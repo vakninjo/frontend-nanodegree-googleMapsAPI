61ed1b120eeb849b261062ea7c688c8d14db4a5f
Jonathan Vaknin <vakninjo@gmail.com>
2018-11-06 15:02:05 -0600

    Map connector with google-maps-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {GoogleApiWrapper} from 'google-maps-react';
+import MapCanvas from './components/MapCanvas';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="app-header">
+          <h1>Sushi Finder - Frisco, TX</h1>
+        </div>
+        <MapCanvas 
+
+        />
+
       </div>
     );
   }
